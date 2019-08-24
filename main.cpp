6ceb03f15f1c881bbfc66e024c5a11bda714376e
Jaden Antoine <jadenjantoine@gmail.com>
2019-08-24 13:27:56 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+#define NR_ROWS 3
+#define NR_COLUMNS 3
+
+chr gameBoard[NR_ROWS][NR_COLUMNS];// the game board
+
+void ClearBoard();//clears the game board
+
+void PrintBoard();//displays board after each move
+
+void Input(int position_x, int position_y);/allows user to input their pic at a specific point
+
+void Winner();//checks whether there are 3 in a row for either the Ai or user
+
+
+
+int main(){
+
+}
