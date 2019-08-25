a772d6925f3d1b5a08ee86037fb40ca4716d9b72
Jaden Antoine <jadenjantoine@gmail.com>
2019-08-24 20:38:51 -0400

    Added and coded all functions
    Check if functions work and complete project by coding a way for the game to start, continue, end, and reset if the user wants to.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 #define NR_ROWS 3
@@ -6,16 +7,193 @@ using namespace std;
 
 chr gameBoard[NR_ROWS][NR_COLUMNS];// the game board
 
-void ClearBoard();//clears the game board
+struct position{
+  int x;
+  int y;
+};
 
-void PrintBoard();//displays board after each move
+void CreateBoard(){  ////creates a new game board
+  for(int i = 0; i < NR_ROWS; i++){
+     for(int j = 0; j < NR_COLUMNS; j++){
+        gameBoard[i][j] = " ";
+        printf("| %c |", gameBoard[i][j]);
+     }
+    cout<< "_______________" << "\n";
+  }
+  
+  PrintBoard();
+}
 
-void Input(int position_x, int position_y);/allows user to input their pic at a specific point
+void PrintBoard(){  //displays board after each move
+  for(int i = 0; i < NR_ROWS; i++){
+    for(int j = 0; j < NR_COLUMNS; j++){
+      printf( "| %c |", gameBoard[i][j]);
+    }
+   cout << "_______________" << "\n";
+  }
+  
+}
 
-void Winner();//checks whether there are 3 in a row for either the Ai or user
+void Check_if_Full_Board(){ //checks if the board is full after both moves
+  int full_spaces;
+  
+  for(int i = 0; i < NR_ROWS; i++){
+    for(int j = 0; j < NR_COLUMNS; j++){
+      if(gameBoard[i][j] == "| X |" || gameBoard[i][j] == "| O |")
+        full_spaces++;
+    }
+  }
+  
+  if(full_spaces == 9)
+    return 1;
+  else
+    return 0;
+}
 
+void Input(position User){  //allows User to input their character at a specific point
+    gameBoard[User.x][User.y] = "| X |";
+    PrintBoard();
+}
 
+//case 1: horizontal win
+int checkHorizontal(){  //checks whether there are 3 in a row for either the Ai or User
+ 
+  if( gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][1] == gameBoard[0][2] ){
+    if(gameBoard[0][1] == "| X |"){
+      cout << "CONGRATULATIONS YOU WIN \n";
+      return 1;
+    }
+    
+    else if(gameBoard[0][1] == "| O |"){
+      cout << "YOU LOSE \n";
+      return 0;
+    }
+    
+    else
+      break;
+  }
+  
+  else if(gameBoard[1][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[1][2]){
+    if(gameBoard[1][1] == "| X |"){
+      cout << "CONGRATULATIONS YOU WIN \n";
+      return 1;
+    }
+    
+    else if(gameBoard[1][1] == "| O |"){
+      cout << "YOU LOSE \n";
+      return 0;
+    }
+    
+    else
+      break;
+  }
+  
+  else if(gameBoard[2][0] == gameBoard[2][1] && gameBoard[2][1] == gameBoard[2][2]){
+    if(gameBoard[2][1] == "| X |"){
+      cout << "CONGRATULATIONS YOU WIN \n";
+      return 1;
+    }
+    
+    else if(gameBoard[2][1] == "| O |"){
+      cout << "YOU LOSE \n";
+      return 0;
+    }
+    
+    else
+      break;
+  }
+  
+  return 2; //no winner
+}
+
+//case 2: vertical win
+int checkVertical(){ //checks whether there are 3 in a row for either the Ai or user
+  if( gameBoard[0][0] == gameBoard[1][0] && gameBoard[1][0] == gameBoard[2][0] ){
+    if(gameBoard[1][0] == "| X |"){
+      cout << "CONGRATULATIONS YOU WIN \n";
+      return 1;
+    }
+    
+    else if(gameBoard[1][0] == "| O |"){
+      cout << "YOU LOSE \n";
+      return 0;
+    }
+    
+    else
+      break;
+  }
+  
+  else if(gameBoard[0][1] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][1]){
+    if(gameBoard[1][1] == "| X |"){
+      cout << "CONGRATULATIONS YOU WIN \n";
+      return 1;
+    }
+    
+    else if(gameBoard[1][1] == "| O |"){
+      cout << "YOU LOSE \n";
+      return 0;
+    }
+    
+    else
+      break;
+  }
+  
+  else if(gameBoard[0][2] == gameBoard[1][2] && gameBoard[1][2] == gameBoard[2][2]){
+    if(gameBoard[1][2] == "| X |"){
+      cout << "CONGRATULATIONS YOU WIN \n";
+      return 1;
+    }
+    
+    else if(gameBoard[1][2] == "| O |"){
+      cout << "YOU LOSE \n";
+      return 0;
+    }
+    
+    else
+      break;
+  }
+  
+  return 2; // no winner
+}
+
+//case 3: a cross win
+int checkAcross(){ //checks whether there are 3 in a row for either the Ai or user
+  if( gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2] ){
+    if(gameBoard[1][1] == "| X |"){
+      cout << "CONGRATULATIONS YOU WIN \n";
+      return 1;
+    }
+    
+    else if(gameBoard[1][1] == "| O |"){
+      cout << "YOU LOSE \n";
+      return 0;
+    }
+    
+    else
+      break;
+  }
+  
+  else if(gameBoard[2][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[0][2]){
+    if(gameBoard[1][1] == "| X |"){
+      cout << "CONGRATULATIONS YOU WIN \n";
+      return 1;
+    }
+    
+    else if(gameBoard[1][1] == "| O |"){
+      cout << "YOU LOSE \n";
+      return 0;
+    }
+    
+    else
+      break;
+  }
+  
+  return 2; //no winner
+}
 
 int main(){
-
+  
+  /* return 0 = Ai win 
+     return 1 = User win
+     return 2 = no one won yet */
 }
